2cb4cdf2d5df57e491ce0a579834e3af47d6682e
DuuliVazi <37152049+DuuliVazi@users.noreply.github.com>
2022-08-18 15:29:19 -0700

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,17 @@
+using System;
+using MyUtilities;
+
+namespace MyFirstProgram
+{
+
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where should we go before the summer?");
+            WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
+            WeatherUtilities.Report("Bologna", 23, 65); // ~73 F
+        }
+    }
+}
